36a9207b0b14a3f992ea4abee60a0e49f9796719
paulhunter <p.hunter.eng@gmail.com>
2015-03-23 02:48:07 -0700

    ray definition

---
+++ RayTracer/ray.h
@@ -0,0 +1,59 @@
+/**
+ * ray.h 
+ * A parametric line construct and encapsulated factory methods used by
+ * the RayTracer to generate an image. 
+ *
+ */
+
+ #ifndef RAY_H
+ #define RAY_H
+
+#include <QVector3D>
+
+class Ray
+{
+public:
+	/** Constructor
+	 * Construct the ray base assuming a Y-axis up vector. 
+	 */
+	Ray(QVector3D eyePos, QVector3D direction);
+
+	/** setToOrthographicRay
+	 * Given the focal length of the camera, screen height/width and the pixel indices,
+	 * set the structure to the appropriate ray from the center of that pixel. 
+	 */
+	void setToOrthographicRay(
+		double focalLength, 
+		double screenWidth, 
+		double screenHeight, 
+		double i, 
+		double j);
+
+	/** setToPerspectiveRay
+	 * Given the focal length of the camera, screen height/width and the pixel indicies, 
+	 * set the structure to the appropriate ray through the centre of that pixel. 
+	 */
+	void setToPerspectiveRay(
+		double focalLength,
+		double screenWidth, 
+		double screenHeight, 
+		double i, 
+		double j);
+
+	/** getPoint
+	 * Get a specific point along the parametric line underlying the ray.
+	 */
+	QVector3D getPoint(double t);
+
+	/** origin of the ray */
+	QVector3D origin;
+	/* direction from origin of the array */
+	QVector3D direction;
+	/* camera coodinate system */
+	QVector3D u;
+	QVector3D v;
+	QVector3D w;
+
+};
+
+ #endif
