18fde9feb1de2e41ab9a5de813896621fcdab4f0
paulhunter <p.hunter.eng@gmail.com>
2015-03-23 18:01:27 -0700

    SceneObjectProperties created.

---
+++ RayTracer/sceneobjectprops.cpp
@@ -0,0 +1,27 @@
+#include "sceneobjectprop.h"
+
+SceneObjectProperties::SceneObjectProperties()
+{
+	color = QVector3D(0.7, 0.7, 0.7);
+	shine = 100;
+	specularCoef = QVector3D(0.5, 0.5, 0.5);
+	ambientColour = QVector3D(0.3,0.3,0.3);
+	refractiveIndex = 0;
+	mirror = 1;
+}
+
+SceneObjectProperties::SceneObjectProperties(
+		QVector3D colour, 
+		double shine,
+		QVector3D specularCoef
+		QVector3D ambientColour,
+		double refractiveIndex,
+		double mirror)
+{
+	this->colour = colour;
+	this->shine = shine;
+	this->specularCoef = specularCoef;
+	this->ambientColour = ambientColour;
+	this->refractiveIndex = refractiveIndex;
+	this->mirror = mirror;	
+}
---
+++ RayTracer/sceneobjectprops.h
@@ -0,0 +1,36 @@
+/**
+ * sceneobjectprops.h
+ * SceneObjectProperties encapsulate the physical properties of an object which
+ * are then used to determine how light behaves on the surface of that object. 
+ */
+
+#ifndef SCENEOBJECTPROPS_H
+#define SCENEOBJECTPROPS_H
+
+
+
+class SceneObjectProperties
+{
+public:
+	SceneObjectProperties();
+	
+	SceneObjectProperties(
+		QVector3D colour, 
+		double shine,
+		QVector3D specularCoef
+		QVector3D ambientColour,
+		double refractiveIndex,
+		double mirror);
+
+	QVector3D colour;
+	double shine;
+	QVector3D specularCoef;
+	QVector3D ambientColour;
+	double refractiveIndex;
+	double mirror;
+};
+
+
+#endif
+
+
