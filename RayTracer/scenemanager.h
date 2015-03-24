9e5fd694501215777bc49a35c807f21fd93673a6
paulhunter <p.hunter.eng@gmail.com>
2015-03-23 20:26:12 -0700

    Scene Manager start.

---
+++ RayTracer/scenemanager.h
@@ -0,0 +1,22 @@
+/**
+ * scenemanager.h
+ * The Scene Manager is used to construct and interact with a scene. It contains
+ * the scene graph and list of light sources. 
+ */
+
+ #ifndef SCENEMANAGER_H
+ #define SCENEMANAGER_H
+
+class
+{
+public:
+
+
+	SceneGraphNode *root;
+	std::vector<LightSource> lights;
+
+
+};
+
+
+ #endif
