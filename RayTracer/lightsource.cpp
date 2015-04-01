cf49d0f5a210174ab45d438e8e07e20ea4c9b4a1
Paul Hunter <p.hunter.eng@gmail.com>
2015-04-01 10:17:35 -0700

    Stubbing out the lights into a point source for the moment

---
+++ RayTracer/lightsource.cpp
@@ -0,0 +1,15 @@
+#include "lightsouce.h"
+
+Lightsource::LightSource()
+{
+	this->position = QVector3D(0,10,0);
+	this->intensity = 1.0;
+	this->ambientIntensity = 0.5;
+}
+
+LightSource::LightSource(QVector3D position, double intensity, double ambientIntensity)
+{
+	this->position = position;
+	this->intensity = intensity;
+	this->ambientIntensity = ambientIntensity;
+}
--- RayTracer/lightsource.h
+++ RayTracer/lightsource.h
@@ -14,15 +14,17 @@
 class LightSource
 {
 public:
+	/** A 'standard' white light source at 0,10,0; */
+	LightSource();
 	LightSource(QVector3D position, double intensity, double ambientIntensity);
 
-	QVector3D getLightDirection(QVector3D destination);
-	double intersect(Ray ray);
+	//QVector3D getLightDirection(QVector3D destination);
+	//double intersect(Ray ray);
 
 	QVector3D position;
 	double intensity;
 	double ambientIntensity;
-	SceneObject *source;
+	//SceneObject *source;
 };
 
 
