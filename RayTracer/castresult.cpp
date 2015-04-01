979f8dcdaa4f486ca8f915d72b9c3cc9f4ec18e5
phunter@uvic.ca <phunter@ecs354-20.csc.uvic.ca>
2015-04-01 11:37:26 -0700

    Addition of cast result, which contains needed information for shaders after casting the ray

---
+++ RayTracer/castresult.cpp
@@ -0,0 +1,15 @@
+#include "castresult.h"
+
+CastResult::CastResult()
+{
+    this->t = -1; //No hit.
+    this->subject = NULL;
+    this->surfaceNormal = QVector3D(0,0,0);
+}
+
+void CastResult::reset()
+{
+    this->t = -1; //No hit.
+    this->subject = NULL;
+    this->surfaceNormal = QVector3D(0,0,0);
+}
---
+++ RayTracer/castresult.h
@@ -0,0 +1,32 @@
+/**
+ * castresult.h
+ * CastResults are encapsulations of the results of casting a ray into a scene.
+ * They contain information such as the object hit, the t value of interaction,
+ * where in space contact was made, and the normal of the surface contacted.
+ *
+ * This information is then used to calculate shading.
+ */
+
+#ifndef CASTRESULT_H
+#define CASTRESULT_H
+
+#include <QVector3D>
+#include <scenegraphnode.h>
+
+class SceneGraphNode;
+
+class CastResult
+{
+public:
+    CastResult();
+
+    /** Reset the cast result to be used again */
+    void reset();
+
+    double t;
+    SceneGraphNode *subject;
+    QVector3D surfacePoint;
+    QVector3D surfaceNormal;
+};
+
+#endif // CASTRESULT_H
