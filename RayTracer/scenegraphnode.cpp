3341834ee8adc892c2b9d6b6891d2d065ea6726d
Paul Hunter <p.hunter.eng@gmail.com>
2015-04-11 22:02:46 -0700

    Bug Fix - Black Plague

--- RayTracer/scenegraphnode.cpp
+++ RayTracer/scenegraphnode.cpp
@@ -1,6 +1,10 @@
 #include "scenegraphnode.h"
 #include <QDebug>
 
+#ifndef EPSILON
+#define EPSILON 0.0001f
+#endif
+
 SceneGraphNode::SceneGraphNode(SceneObject *object, SceneObjectProperties *material)
 {
     this->sceneObject = object;
@@ -32,6 +36,10 @@ bool SceneGraphNode::castRay(Ray ray, QMatrix4x4 transform, CastResult *result)
             result->t = r;
             result->surfacePoint = ray.origin + r*ray.direction;
             result->surfaceNormal = this->sceneObject->getNormal(result->surfacePoint, transform);
+            //As we discussed in class, the 'Black Plague' occurs when points on a surface are
+            //calculated and as a result of precision errors it ends up calculated as behind
+            //the surface. The solution is to move 0.1mm off the surface using the normal vector. 
+            result->surfacePoint = EPSILON * result->surfaceNormal;
         }
     }
 
