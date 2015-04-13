d899102333dd7630d156049192f2fb54a9c84ba4
Paul Hunter <p.hunter.eng@gmail.com>
2015-04-13 00:10:24 -0700

    Composition - Object Factory helpers added

--- RayTracer/sceneobjectfactory.cpp
+++ RayTracer/sceneobjectfactory.cpp
@@ -9,11 +9,31 @@ SceneObject* SceneObjectFactory::createSceneObjectAtPos(PrimitiveType type, QVec
 }
 
 SceneObject* SceneObjectFactory::createSceneObject_wScaleRot(PrimitiveType type, 
-	QVector3D position, double, scale, double x, double y, double z)
+	QVector3D position, double scale, double x, double y, double z)
 {
 	SceneObject* result = new SceneObject();
 	result->primitive = getPrimitive(type);
 	result->transform.scale(scale);
+	result->transform.rotate(x, 1,0,0);
+	result->transform.rotate(y, 0,1,0);
+	result->transform.rotate(z, 0,0,1);
+	result->transform.translate(position);
+}
+
+SceneObject* SceneObjectFactory::createSceneObject_wScale(PrimitiveType type, 
+	QVector3D position, double scale)
+{
+	SceneObject* result = new SceneObject();
+	result->primitive = getPrimitive(type);
+	result->transform.scale(scale);
+	result->transform.translate(position);
+}
+
+SceneObject* SceneObjectFactory::createSceneObject_wRot(PrimitiveType type, 
+	QVector3D position, double x, double y, double z)
+{
+	SceneObject* result = new SceneObject();
+	result->primitive = getPrimitive(type);
 	result->transform.rotate(x, 1,0,0);
 	result->transform.rotate(y, 0,1,0);
 	result->transform.rotate(z, 0,0,1);
--- RayTracer/sceneobjectfactory.h
+++ RayTracer/sceneobjectfactory.h
@@ -30,9 +30,15 @@ public:
 	/* Create a basic object at the given position in space. */
 	static SceneObject* createSceneObject(PrimitiveType type, QVector3D position);
 
+	/* Create a scaled primitive at the given position in space .*/
+	static SceneObject* createSceneObject_wScale(PrimitiveType type, QVector3D position, double scale);
+
+	/* Create a rotated primitive with original scale */
+	static SceneObject* createSceneObject_wRot(PrimitiveType type, QVector3D position, double x, double y, double z);
+
 	/* Create a primitive, scale it by a factor, rotate it about the x, y, z
 	 * axes, in that order, finally translating it to the position */
-	static SceneObject* createSceneObject_wRot(PrimitiveType type, QVector3D position, double scale, double x, double y, double z);
+	static SceneObject* createSceneObject_wScaleRot(PrimitiveType type, QVector3D position, double scale, double x, double y, double z);
 
 private:
 
